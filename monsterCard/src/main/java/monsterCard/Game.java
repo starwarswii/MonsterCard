0a57f8e07711a42eb744ed98b87d8f32bf61b394
farrer <farrer@rpi.edu>
2019-03-18 15:05:35 -0400

    Added function to change the active players

--- monsterCard/src/main/java/monsterCard/Game.java
+++ monsterCard/src/main/java/monsterCard/Game.java
@@ -2,8 +2,10 @@ package monsterCard;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class Game {
 	String gameName;
@@ -11,7 +13,8 @@ public class Game {
 	List<Spectator> spectators;
 	List<Card> deck;
 	int votes1, votes2, gameID;
-	Player activeplayer1, activeplayer2;
+	String activeplayer1, activeplayer2;
+	List<String> alreadygone;
 	
 	//Default constructor, requires a user provided name for the game and an internally generated game ID
 	public Game(String name, int ID) {
@@ -23,6 +26,7 @@ public class Game {
 	    votes1 = votes2 = 0;
 	    activeplayer1 = null;
 	    activeplayer2 = null;
+	    alreadygone = new ArrayList<String>();
 	}
 	
 	//Adds a new Player with a user provided username
@@ -35,6 +39,30 @@ public class Game {
 	public void addSpectator(String name, String ID) {
 		Spectator newSpectator = new Spectator(name,ID);
 		spectators.add(newSpectator);
+	}
+	
+	public void changeActivePlayers() {
+		if(alreadygone.size()==players.size()) {
+			this.quit();
+		}
+		Random rand = new Random();
+		int i = 100;
+		String[] ids = (String[]) players.keySet().toArray();
+		do {
+			i = rand.nextInt(players.size());
+			if(!alreadygone.contains(ids[i])) {
+				alreadygone.add(ids[i]);
+				activeplayer1 = ids[i];
+				break;
+			}
+		}while(true);
+		do {
+			i = rand.nextInt(players.size());
+			if(!alreadygone.contains(ids[i])) {
+				alreadygone.add(ids[i]);
+				activeplayer2 = ids[i];
+			}
+		}while(true);
 	}
 	
 	//End the game, and return the game ID to GameManager for it to use to remove this game from the list of games
